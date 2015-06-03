9bf6be1247ffd37cc3440812ae8332c56836a86a
tchan247 <tchan247@gmail.com>
2015-06-03 15:09:33 -0700

    Complete tests 1-4

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,35 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var fs = require('fs');
 
 exports.handleRequest = function (req, res) {
+  if (req.method === 'GET') {
+    if (req.url === '/') {
+      res.writeHead(200);
+      res.write('<input>');
+    } else {
+      res.write('<div>google</div>')
+    }
+  }
+
+  fs.readFile( archive.paths.list, function (err, data) {
+    var body = "";
+    if (err) throw err;
+    // console.log(typeof JSON.parse(data));
+    body += data;
+    console.log(body);
+  });
+
+  if(req.method === 'POST'){
+    res.writeHead(302);
+    console.log(req._postData);
+    fs.appendFile( archive.paths.list, req._postData.url + "\n", function (err) {
+      if (err) throw err;
+      console.log('The "data to append" was appended to file!');
+    });
+  }
+
   res.end(archive.paths.list);
 };
+
